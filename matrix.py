830ee93ee78455bedf681373543c8ddeb6b22772
danorel <mail.ordan@gmail.com>
2022-11-03 23:15:12 +0200

    Implement squared matrix multiplication MapReduce pipeline

--- matrix.py
+++ matrix.py
@@ -1,15 +1,83 @@
 from abc import ABC
 
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 
 class MRMatrixMultiplication(MRJob, ABC):
-    def mapper(self, _, line):
-        
-        yield "lines", 1
+    def steps(self):
+        return [
+            MRStep(
+                mapper=self.mapper_matrix_row_col_split,
+                reducer=self.reducer_matrix_row_col_filter
+            ),
+            MRStep(
+                mapper=self.mapper_matrix_row_col_join,
+                reducer=self.reducer_matrix_row_col_dot_product
+            ),
+            MRStep(
+                mapper=self.mapper_row_convolution,
+                reducer=self.reducer_row_convolution
+            ),
+            MRStep(
+                mapper=self.mapper_matrix_convolution,
+                reducer=self.reducer_matrix_convolution
+            )
+        ]
 
-    def reducer(self, key, values):
-        yield key, sum(values)
+    @staticmethod
+    def mapper_matrix_row_col_split(_, line):
+        number, representation = line.split(":")
+        rows = representation.split("|")
+        for i, row in enumerate(rows):
+            yield f"matrix-{number}:row-{i}", row.split(",")
+        for j in range(len(rows)):
+            column = [row.split(",")[j] for row in rows]
+            yield f"matrix-{number}:col-{j}", column
+
+    @staticmethod
+    def reducer_matrix_row_col_filter(position, values):
+        if "matrix-1:row" in position or "matrix-2:col" in position:
+            yield position, next(values)
+
+    @staticmethod
+    def mapper_matrix_row_col_join(position, values):
+        if "matrix-2:col" in position:
+            index_is_column = position[13]
+            for index_for_row in range(len(values)):
+                yield f"row-{index_for_row}:column-{index_is_column}", values
+        else:
+            index_is_row = position[13]
+            for index_for_col in range(len(values)):
+                yield f"row-{index_is_row}:column-{index_for_col}", values
+
+    @staticmethod
+    def reducer_matrix_row_col_dot_product(location, values):
+        row = next(values)
+        column = next(values)
+        dot_product = []
+        for r_value, c_value in zip(row, column):
+            dot_product.append(int(r_value) * int(c_value))
+        yield location, sum(dot_product)
+
+    @staticmethod
+    def mapper_row_convolution(position, value):
+        row_number = position[4]
+        row_column_value = str(value)
+        yield row_number, row_column_value
+
+    @staticmethod
+    def reducer_row_convolution(row_number, row_column_values):
+        row_column_values = ','.join(row_column_values)
+        yield row_number, row_column_values
+
+    @staticmethod
+    def mapper_matrix_convolution(_, row_values):
+        yield "matrix", row_values
+
+    @staticmethod
+    def reducer_matrix_convolution(matrix, row_values):
+        yield matrix, '|'.join(row_values)
 
 
 if __name__ == '__main__':
--- vector.py
+++ vector.py
@@ -20,23 +20,23 @@ class MRVectorScalarMultiplication(MRJob, ABC):
     @staticmethod
     def mapper_pair_representation(_, line):
         values = line.split(",")
-        for i, value in enumerate(values):
-            yield i, value
+        for index, value in enumerate(values):
+            yield index, value
 
     @staticmethod
-    def reducer_pair_dot_product(key, values):
+    def reducer_pair_dot_product(index, values):
         pair_dot_product = 1
         for value in values:
             pair_dot_product *= float(value)
-        yield key, pair_dot_product
+        yield index, pair_dot_product
 
     @staticmethod
     def mapper_vector_representation(_, pair_dot_product):
         yield "result", pair_dot_product
 
     @staticmethod
-    def reducer_vector_dot_product(key, pair_dot_product):
-        yield key, sum(pair_dot_product)
+    def reducer_vector_dot_product(result, pair_dot_product):
+        yield result, sum(pair_dot_product)
 
 
 if __name__ == '__main__':
